6fd9a593b847cd6f1d5861da74092fff433c49fd
LAPTOP-LK4OIR7G\niek_ <niek_riggeling@hotmail.com>
2019-10-13 12:44:01 +0200

    form data, string, array, math functions

---
+++ demo/math_function.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    // macht van
+    echo pow(2,7);
+
+    echo "<br>";
+
+    echo rand(1, 1000);
+
+    echo "<br>";
+
+    // square root
+    echo sqrt(100);
+
+    echo "<br>";
+
+    // round up
+    echo ceil(4.6);
+
+    echo "<br>";
+
+    // round down
+    echo floor(4.6);
+
+    echo "<br>";
+
+
+
+    ?>
+</body>
+</html>
